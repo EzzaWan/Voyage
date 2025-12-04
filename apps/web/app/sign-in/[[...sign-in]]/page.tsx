ad9ac32381e1162b347abbe2124716278c3d80d4
Ezza Wan <ezzawan9@gmail.com>
2025-12-04 17:33:14 +0100

    Hide OAuth providers in sign-in page to fix Google OAuth error

--- apps/web/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,18 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="flex items-center justify-center min-h-[80vh]">
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            rootBox: "mx-auto",
+            card: "bg-[var(--voyage-card)] border-[var(--voyage-border)]",
+            socialButtonsBlockButton: "hidden", // Hide OAuth buttons
+            socialButtonsBlockButtonText: "hidden",
+            dividerLine: "hidden",
+            dividerText: "hidden",
+          },
+        }}
+      />
     </div>
   );
 }
