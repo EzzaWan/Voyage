c715a79ef6e91ea030ace24831207624c5e79f3a
Ezza Wan <ezzawan9@gmail.com>
2025-12-05 18:14:53 +0100

    Fix build error: Export CountrySkeleton from skeletons module

--- apps/web/components/skeletons/index.ts
+++ apps/web/components/skeletons/index.ts
@@ -2,5 +2,6 @@ export { LoadingCountryList } from "./LoadingCountryList";
 export { LoadingPlanList } from "./LoadingPlanList";
 export { LoadingEsimCard } from "./LoadingEsimCard";
 export { LoadingCheckout } from "./LoadingCheckout";
+export { CountrySkeleton, PlanCardSkeleton, PlanDetailsSkeleton } from "./SkeletonComponents";
 
 
